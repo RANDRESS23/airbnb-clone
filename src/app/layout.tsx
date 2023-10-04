9a9527359c54b3d9ea54cf13b1b8c76a8708169a
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-10-03 23:59:56 -0500

    Adding the application NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar/NavBar'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout ({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <NavBar />
+
+        {children}
+      </body>
     </html>
   )
 }
